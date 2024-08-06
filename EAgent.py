6647f0ef726aa0ad4620e268bd3809a4939cc37a
Bryan(Yu) Zhou <bryanzhou008@gmail.com>
2024-08-06 15:35:45 -0700

    test

--- EAgent.py
+++ EAgent.py
@@ -1,5 +1,5 @@
 """
-Lumache - Python library for cooks and food lovers.
+EAgent
 """
 
 __version__ = "0.1.0"
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'EAgent'
+copyright = '2024, Yu ZHou'
+author = 'Yu Zhou'
 
 release = '0.1'
 version = '0.1.0'

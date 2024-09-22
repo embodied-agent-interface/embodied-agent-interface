5c2a29f31ed7d180808b6a92f8e3a14c096071bd
Kangrui Wang <kangruiwang.cs@gmail.com>
2024-09-22 11:34:10 -0700

    put packcage under /src

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='eagent-eval',
     version='0.0.1',
-    packages=find_packages(),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
     python_requires='>=3.8',
     install_requires=[
         "lark",

56efa93924e88e7d22f1d1139de0967c68a00c96
Kangrui Wang <krwang2022@gmail.com>
2024-09-24 21:43:32 -0500

    added init files for data and resources directories

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='eagent-eval',
-    version='0.0.3',
+    version='0.0.4',
     author='stanford',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",

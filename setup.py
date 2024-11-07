4ce4b52effc9d62a9a155efa56be9b53324252f9
Kangrui Wang <krwang2022@gmail.com>
2024-11-07 01:36:28 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='eai-eval',
-    version='1.0.3',
+    version='1.0.4',
     author='stanford',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",

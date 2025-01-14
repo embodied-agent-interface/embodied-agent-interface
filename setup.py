bd1048d439dc6c6290e934416936b57f461b01f8
Shiyu Zhao <61644849+zsyJosh@users.noreply.github.com>
2025-01-13 21:30:03 -0800

    Update Package 1.0.4 -> 1.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='eai-eval',
-    version='1.0.4',
+    version='1.0.5',
     author='stanford',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
